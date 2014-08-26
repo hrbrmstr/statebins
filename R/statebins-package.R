11ab36843485c50916a59b2dc4b0d75e32cfd9c9
Bob Rudis <bob@rudis.net>
2014-08-26 08:44:02 -0400

    modified continuous scale version + added docs

--- R/statebins-package.R
+++ R/statebins-package.R
@@ -1,5 +1,5 @@
 #' statebins is an alternative to choropleth maps for US States
-#' @name statebins
+#' @name statebins-package
 #' @docType package
 #' @author Bob Rudis (@@hrbrmstr)
 #' @import ggplot2 scales grid gridExtra RColorBrewer
