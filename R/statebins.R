dc631dbe51180c98b8aebe31624c107096688c94
hrbrmstr <bob@rudis.net>
2014-08-26 17:46:33 -0400

    Updating for CRAN submission

--- R/statebins.R
+++ R/statebins.R
@@ -1,3 +1,29 @@
+state_coords <- structure(list(abbrev = c("AL", "AK", "AZ", "AR", "CA", "CO",
+                                          "CT", "DC", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS",
+                                          "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE",
+                                          "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA",
+                                          "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"),
+                               state = c("Alabama", "Alaska", "Arizona", "Arkansas",
+                                         "California", "Colorado", "Connecticut", "District of Columbia",
+                                         "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois",
+                                         "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine",
+                                         "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
+                                         "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
+                                         "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota",
+                                         "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island",
+                                         "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
+                                         "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"),
+                               col = c(8L, 2L, 3L, 6L, 2L, 4L, 11L, 10L, 11L, 10L,
+                                       9L, 1L, 3L, 7L, 7L, 6L, 5L, 7L, 6L, 12L, 10L, 11L, 8L, 6L, 7L,
+                                       6L, 4L, 5L, 3L, 12L, 10L, 4L, 10L, 8L, 5L, 8L, 5L, 2L, 9L, 12L,
+                                       9L, 5L, 7L, 5L, 3L, 11L, 9L, 2L, 8L, 7L, 4L),
+                               row = c(7L, 8L,
+                                       6L, 6L, 5L, 5L, 4L, 6L, 5L, 8L, 7L, 8L, 3L, 3L, 4L, 4L, 6L, 5L,
+                                       7L, 1L, 5L, 3L, 3L, 3L, 7L, 5L, 3L, 5L, 4L, 2L, 4L, 6L, 3L, 6L,
+                                       3L, 4L, 7L, 4L, 4L, 4L, 6L, 4L, 6L, 8L, 5L, 2L, 5L, 3L, 5L, 2L, 4L)),
+                          .Names = c("abbrev", "state", "col", "row"), class = "data.frame", row.names = c(NA, -51L))
+
+
 invert <- function(hexColor, darkColor="black", lightColor="white") {
 
   hexColor <- gsub("#", "", hexColor)
@@ -18,7 +44,7 @@ invert <- function(hexColor, darkColor="black", lightColor="white") {
 
 }
 
-#' Create a new ggplot-based "statebin" chart
+#' Create a new ggplot-based "statebin" chart for USA states (discrete scale)
 #'
 #' \code{statebins()} creates "statebin" charts in the style of \url{http://bit.ly/statebins}
 #'
@@ -37,7 +63,7 @@ invert <- function(hexColor, darkColor="black", lightColor="white") {
 #' and \code{value_col} accordingly.
 #'
 #' To add a title, change \code{plot_title} to anything but an empty atomic string vector (i.e. \code{""})
-#' and set \code{title_positioin} to "\code{top}" or "\code{bottom}". Choosing "\code{bottom}"
+#' and set \code{title_position} to "\code{top}" or "\code{bottom}". Choosing "\code{bottom}"
 #' will cause \code{statebins} to use \link{arrangeGrob} to position the title via \code{sub} and
 #' return a frame grob instead of a ggplot2 object.
 #'
@@ -45,7 +71,7 @@ invert <- function(hexColor, darkColor="black", lightColor="white") {
 #' @param state_col column name in \code{state_data} that has the states. no duplicates and can be names (e.g. "\code{Maine}") or abbreviatons (e.g. "\code{ME}")
 #' @param value_col column name in \code{state_data} that holds the values to be plotted
 #' @param text_color default "\code{white}"
-#' @param font_size font size (default = \code{2})
+#' @param font_size font size (default = \code{3})
 #' @param state_border_col default "\code{white}" - this creates the "spaces" between boxes
 #' @param breaks a single number (greater than or equal to 2) giving the number of intervals into which data values are to be cut.
 #' @param labels labels for the levels \code{breaks}
@@ -56,8 +82,15 @@ invert <- function(hexColor, darkColor="black", lightColor="white") {
 #' @param title_position where to put the title ("\code{bottom}" or "\code{top}" or "" for none); if "\code{bottom}", you get back a grob vs a ggplot object
 #' @return ggplot2 object or grob
 #' @export
+#' @examples
+#' \dontrun{
+#' data(USArrests)
+#' USArrests$state <- rownames(USArrests)
+#' statebins(USArrests, value_col="Assault", text_color="black", font_size=3,
+#'           legend_title = "Assault", legend_position="bottom")
+#' }
 statebins <- function(state_data, state_col="state", value_col="value",
-                     text_color="white", font_size=2,
+                     text_color="white", font_size=3,
                      state_border_col="white", breaks=5, labels=1:5,
                      legend_title="Legend", legend_position="top",
                      brewer_pal="PuBu", plot_title="", title_position="bottom") {
@@ -88,8 +121,6 @@ statebins <- function(state_data, state_col="state", value_col="value",
   gg <- gg + coord_equal()
   gg <- gg + labs(x=NULL, y=NULL, title=NULL)
   gg <- gg + theme_bw()
-  gg <- gg + theme(plot.margin=unit(c(-1, 0, 0, 0), "line"))
-  gg <- gg + theme(panel.margin=unit(-1, "line"))
   gg <- gg + theme(legend.position=legend_position)
   gg <- gg + theme(panel.border=element_blank())
   gg <- gg + theme(panel.grid=element_blank())
@@ -112,7 +143,7 @@ statebins <- function(state_data, state_col="state", value_col="value",
 }
 
 
-#' Create ggplot-based "statebin" charts in the style of \url{http://bit.ly/statebins}
+#' Create a new ggplot-based "statebin" chart for USA states (continuous scale)
 #'
 #' \code{statebins()} creates "statebin" charts in the style of \url{http://bit.ly/statebins}
 #'
@@ -132,7 +163,7 @@ statebins <- function(state_data, state_col="state", value_col="value",
 #' and \code{value_col} accordingly.
 #'
 #' To add a title, change \code{plot_title} to anything but an empty atomic string vector (i.e. \code{""})
-#' and set \code{title_positioin} to "\code{top}" or "\code{bottom}". Choosing "\code{bottom}"
+#' and set \code{title_position} to "\code{top}" or "\code{bottom}". Choosing "\code{bottom}"
 #' will cause \code{statebins} to use \link{arrangeGrob} to position the title via \code{sub} and
 #' return a frame grob instead of a ggplot2 object.
 #'
@@ -140,7 +171,7 @@ statebins <- function(state_data, state_col="state", value_col="value",
 #' @param state_col column name in \code{state_data} that has the states. no duplicates and can be names (e.g. "\code{Maine}") or abbreviatons (e.g. "\code{ME}")
 #' @param value_col column name in \code{state_data} that holds the values to be plotted
 #' @param text_color default "\code{white}"
-#' @param font_size font size (default = \code{2})
+#' @param font_size font size (default = \code{3})
 #' @param state_border_col default "\code{white}" - this creates the "spaces" between boxes
 #' @param legend_title title for the legend
 #' @param legend_position "\code{none}", "\code{top}", "\code{left}", "\code{right}" or "\code{bottom}" (defaults to "\code{top}")
@@ -149,8 +180,15 @@ statebins <- function(state_data, state_col="state", value_col="value",
 #' @param title_position where to put the title ("\code{bottom}" or "\code{top}" or "" for none); if "\code{bottom}", you get back a grob vs a ggplot object
 #' @return ggplot2 object or grob
 #' @export
+#' @examples
+#' \dontrun{
+#' data(USArrests)
+#' USArrests$state <- rownames(USArrests)
+#' statebins_continuous(USArrests, value_col="Murder", text_color="black", font_size=3,
+#'                      legend_title = "Murder", legend_position="bottom")
+#' }
 statebins_continuous <- function(state_data, state_col="state", value_col="value",
-                      text_color="white", font_size=2,
+                      text_color="white", font_size=3,
                       state_border_col="white",
                       legend_title="Legend", legend_position="top",
                       brewer_pal="PuBu", plot_title="", title_position="bottom") {
@@ -179,8 +217,6 @@ statebins_continuous <- function(state_data, state_col="state", value_col="value
   gg <- gg + coord_equal()
   gg <- gg + labs(x=NULL, y=NULL, title=NULL)
   gg <- gg + theme_bw()
-  gg <- gg + theme(plot.margin=unit(c(-1, 0, 0, 0), "line"))
-  gg <- gg + theme(panel.margin=unit(-1, "line"))
   gg <- gg + theme(legend.position=legend_position)
   gg <- gg + theme(panel.border=element_blank())
   gg <- gg + theme(panel.grid=element_blank())
